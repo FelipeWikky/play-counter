00799cb4c956d8ef16711e0144d9bca432ea53f7
Felipe Lima <felipewikky@gmail.com>
2022-06-03 12:00:09 +1000

    feat: added lib to manage modal animated

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,25 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+// // Learn more https://docs.expo.io/guides/customizing-metro
+// const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+// module.exports = getDefaultConfig(__dirname);
+
+const {getDefaultConfig} = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts},
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      sourceExts: [...sourceExts, 'cjs'],
+    },
+  };
+})();
